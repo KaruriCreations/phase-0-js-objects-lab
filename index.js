cbf88b3a15fa25ad7b090e1b6ade49c1704c0938
KaruriCreations <vincent.maina3@student.moringaschool.com>
2026-03-20 16:17:27 +0300

    added logic

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-//Write your code here
+const attendee = {
+  attendeeId: 'T001',
+  name: 'Alice Smith',
+  event: 'JavaScript Conference',
+  ticketType: 'VIP',
+  ticketPrice: 150.00
+};
 
+function logAttendeeName(person) {
+  console.log(person.name);
+}
 
+function logTicketPrice(person) {
+  console.log(person.ticketPrice);
+}
 
-//Needed for the tests to work. Don't modify
+function updateTicketType(person, newType) {
+  person.ticketType = newType;
+}
+
+function updateTicketPrice(person, newPrice) {
+  person.ticketPrice = newPrice;
+}
+
+function removeEventProperty(person) {
+  delete person.event;
+}
+
+function addCheckedInProperty(person) {
+  person.checkedIn = true;
+}
+
+// Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
